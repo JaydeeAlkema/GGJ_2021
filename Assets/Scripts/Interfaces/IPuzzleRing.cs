4235da06d4559e50a1978b73921f24f2ec7255aa
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-31 00:09:55 +0100

    Fix Puzzle with interaction

---
+++ Assets/Scripts/Interfaces/IPuzzleRing.cs
@@ -0,0 +1,4 @@
+﻿public interface IPuzzleRing
+{
+	void Interact();
+}
--- Assets/Scripts/Puzzle 1/Puzzle_Ring.cs
+++ Assets/Scripts/Puzzle 1/Puzzle_Ring.cs
@@ -2,35 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puzzle_Ring : MonoBehaviour
+public class Puzzle_Ring : MonoBehaviour, IPuzzleRing
 {
 	[SerializeField] private int state = 0;
 	[SerializeField] private Animator anim;
-	[SerializeField] private KeyCode changeStateKey;
 	[SerializeField] private AudioClip audioClip = default;
 
 	public int State { get => state; set => state = value; }
 
 	private void Start()
 	{
-		anim = GetComponent<Animator>();
 		state = Random.Range(1, 4);
 		anim.SetInteger("State", state);
-
-	}
-
-	private void Update()
-	{
-		if(Input.GetKeyDown(changeStateKey))
-			if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0))
-				ChangeToNextState();
 	}
 
 	public void ChangeToNextState()
 	{
-		state++;
-		if(state > 4) state = 1;
-		anim.SetInteger("State", state);
-		AudioManager.PlayAudioAtPosition(transform.position, audioClip, 0.15f, false);
+		if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0))
+		{
+			state++;
+			if(state > 4) state = 1;
+			anim.SetInteger("State", state);
+			AudioManager.PlayAudioAtPosition(transform.position, audioClip, 0.15f, false);
+		}
+	}
+
+	public void Interact()
+	{
+		ChangeToNextState();
 	}
 }
