483fbaf40e8369a59e8a121c4de170b70d0928ef
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-30 02:16:57 +0100

    Add Base Audiomanager functionality

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	private static AudioManager instance;
+
+	public static AudioManager Instance { get => instance; set => instance = value; }
+
+	private void Awake()
+	{
+		if(instance != null && instance != this) Destroy(this.gameObject);
+		else instance = this;
+	}
+
+	/// <summary>
+	/// Plays an audioclip at the given position and volume.
+	/// </summary>
+	/// <param name="pos"> Where to play the audio clip. (world coordinates)</param>
+	/// <param name="clip"> Which audioclip to play. </param>
+	/// <param name="volume"> How loud it should be. </param>
+	void PlayAudioAtPosition(Vector3 pos, AudioClip clip, float volume)
+	{
+		AudioSource.PlayClipAtPoint(clip, pos, volume);
+	}
+}
