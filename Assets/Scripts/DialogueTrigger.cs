b7dc8018752bb6a9b010df09705110cbda7f9b8a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-30 14:58:56 +0100

    Add Dialogue Manager and Trigger.

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+	[SerializeField] private string textToDisplay = default;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		DialogueManager.Instance.SetFullText(textToDisplay);
+		DialogueManager.Instance.LetTextAppearWithDelay();
+
+		Destroy(this.gameObject);
+	}
+}
