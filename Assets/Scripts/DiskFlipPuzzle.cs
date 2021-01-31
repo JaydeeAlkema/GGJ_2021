882cffd325662ae90fe3fe085e4f7a65a4439680
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-31 15:42:27 +0100

    fix shit

--- Assets/Scripts/DiskFlipPuzzle.cs
+++ Assets/Scripts/DiskFlipPuzzle.cs
@@ -5,58 +5,67 @@ using UnityEngine;
 public class DiskFlipPuzzle : MonoBehaviour
 {
 
-    [SerializeField] private int totalAmountOfPuzzles = 3;
-    [SerializeField] private int totalPuzzleCompleted = 0;
+	[SerializeField] private int totalAmountOfPuzzles = 3;
+	[SerializeField] private int totalPuzzleCompleted = 0;
 
-    [SerializeField] private List<FlipButton> flipButtons = new List<FlipButton>();
+	[SerializeField] private List<FlipButton> flipButtons = new List<FlipButton>();
 
-    [SerializeField] private int timer = 250;
-    [SerializeField] private bool FlipPuzzleDone = false;
+	[SerializeField] private int timer = 250;
+	[SerializeField] private bool FlipPuzzleDone = false;
 
-    [SerializeField] private List<ParticleSystem> laserParticles = new List<ParticleSystem>();
+	[SerializeField] private List<ParticleSystem> laserParticles = new List<ParticleSystem>();
 
-    void Update()
-    {
-        if (timer > 0)
-        {
-            timer--;
-        } 
-        else
-        {
-            timer = 250;
-            SlowUpdate();
-        }
-    }
+	private void Start()
+	{
+		FlipPuzzleDoneEvent(false);
+	}
 
-    void SlowUpdate()
-    {
-        CheckFlipPuzzle();
-    }
+	void Update()
+	{
+		if(timer > 0)
+		{
+			timer--;
+		}
+		else
+		{
+			timer = 250;
+			SlowUpdate();
+		}
+	}
 
-    private void CheckFlipPuzzle()
-    {
-        int amount = 0;
-        for (int i = 0; i < flipButtons.Count; i++)
-        {
-            if (!flipButtons[i].Flipped)
-            {
-                amount += 1;
-            }
-        }
-        if (amount == flipButtons.Count)
-        {
-            if (!FlipPuzzleDone)
-            {
-                FlipPuzzleDoneEvent();
-                FlipPuzzleDone = true;
-            }
-        }
-    }
-    private void FlipPuzzleDoneEvent()
-    {
-        for (int i = 0; i < laserParticles.Count; i++)
-        {
-            laserParticles[i].Play();
-        }
-    }
+	void SlowUpdate()
+	{
+		CheckFlipPuzzle();
+	}
+
+	private void CheckFlipPuzzle()
+	{
+		int amount = 0;
+		for(int i = 0; i < flipButtons.Count; i++)
+		{
+			if(!flipButtons[i].Flipped)
+			{
+				amount += 1;
+			}
+		}
+		if(amount == flipButtons.Count)
+		{
+			if(!FlipPuzzleDone)
+			{
+				FlipPuzzleDoneEvent(true);
+				FlipPuzzleDone = true;
+			}
+		}
+	}
+	private void FlipPuzzleDoneEvent(bool active)
+	{
+		for(int i = 0; i < laserParticles.Count; i++)
+		{
+			if(active)
+				laserParticles[i].Play();
+			else
+				laserParticles[i].Stop();
+
+		}
+	}
 }
