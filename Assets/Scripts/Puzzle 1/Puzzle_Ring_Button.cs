2abf19098307faba6ee5bd958a361e04da78f1e5
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-31 14:08:36 +0100

    Add Interaction to Puzzle 1

---
+++ Assets/Scripts/Puzzle 1/Puzzle_Ring_Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Ring_Button : MonoBehaviour, IInteractable
+{
+	[SerializeField] private Puzzle_Rotato puzzle_Rotato;
+	public void Interact()
+	{
+		puzzle_Rotato.CheckCircleStates();
+		GetComponent<Animator>().SetTrigger("Pressed");
+	}
+}
--- Assets/Scripts/Puzzle 1/Puzzle_Rotato.cs
+++ Assets/Scripts/Puzzle 1/Puzzle_Rotato.cs
@@ -15,11 +15,36 @@ public class Puzzle_Rotato : MonoBehaviour
 	[SerializeField] private bool circleOuterCorrectlyRotated = false;
 	[SerializeField] private bool circleMiddleCorrectlyRotated = false;
 	[SerializeField] private bool circleInnerCorrectlyRotated = false;
+	[SerializeField] private bool Puzzlecomplete = false;
+	[Space]
+	[SerializeField] private List<ParticleSystem> laserParticles = new List<ParticleSystem>();
 
-	private void FixedUpdate()
+	private void Start()
+	{
+		PuzzleCompleteEvent(Puzzlecomplete);
+	}
+
+	public void CheckCircleStates()
 	{
 		circleOuterCorrectlyRotated = circleOuter.State == circleOuterTargetState;
 		circleMiddleCorrectlyRotated = circleMiddle.State == circleMiddleTargetState;
 		circleInnerCorrectlyRotated = circleInner.State == circleInnerTargetState;
+		if(circleOuterCorrectlyRotated && circleMiddleCorrectlyRotated && circleInnerCorrectlyRotated && !Puzzlecomplete)
+		{
+			Puzzlecomplete = true;
+			PuzzleCompleteEvent(Puzzlecomplete);
+		}
+	}
+
+	private void PuzzleCompleteEvent(bool isComplete)
+	{
+		for(int i = 0; i < laserParticles.Count; i++)
+		{
+			if(isComplete)
+				laserParticles[i].Play();
+			else
+				laserParticles[i].Stop();
+
+		}
 	}
 }
